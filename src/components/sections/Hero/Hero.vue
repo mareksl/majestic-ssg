b88a6c06b8607e3bc0c29f39a33d88dab6d17295
Marek Sladczyk <marek.sladczyk@stxnext.pl>
2020-09-12 22:46:59 +0200

    add logo and icons

--- src/components/sections/Hero/Hero.vue
+++ src/components/sections/Hero/Hero.vue
@@ -1,15 +1,46 @@
 <template>
-  <section id="start">
-    <p>
-      Hero
-    </p>
+  <section id="start" class="hero">
+    <div class="hero__content">
+      <Logo />
+      <SocialLinks />
+    </div>
   </section>
 </template>
 
 <script>
+import Logo from "@/components/sections/Hero/Logo";
+import SocialLinks from "@/components/sections/Hero/SocialLinks";
+
 export default {
-  name: "Hero"
+  name: "Hero",
+  components: { SocialLinks, Logo }
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import "src/styles/variables/dimensions";
+
+.hero {
+  position: relative;
+  display: flex;
+  flex-flow: column;
+  min-height: calc(100vh - 4rem);
+  align-items: center;
+  justify-content: center;
+
+  &__content {
+    max-width: 400px;
+    margin: 0 2rem;
+  }
+}
+
+@media screen and (min-width: $breakpoint-sm) {
+  .hero {
+    &__content {
+      max-width: unset;
+      min-width: 400px;
+      width: 33%;
+    }
+  }
+}
+</style>
---
+++ src/components/sections/Hero/SocialLinks.vue
@@ -0,0 +1,88 @@
+<template>
+  <ul class="social-links">
+    <li class="social-links__item">
+      <a
+        class="social-links__link"
+        href="https://www.facebook.com/majesticreggae/"
+        target="_blank"
+      >
+        <g-image
+          class="social-links__image"
+          src="~/assets/icons/facebook-square.svg"
+          alt="Facebook"
+          title="Facebook"
+        ></g-image>
+      </a>
+    </li>
+    <li class="social-links__item">
+      <a class="social-links__link" href="https://spotify.com/" target="_blank">
+        <g-image
+          class="social-links__image"
+          src="~/assets/icons/spotify.svg"
+          alt="Spotify"
+          title="Spotify"
+        ></g-image>
+      </a>
+    </li>
+    <li class="social-links__item">
+      <a
+        class="social-links__link"
+        href="https://www.youtube.com/user/majesticreggae"
+        target="_blank"
+      >
+        <g-image
+          class="social-links__image"
+          src="~/assets/icons/youtube.svg"
+          alt="Youtube"
+          title="Youtube"
+        ></g-image>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "SocialLinks"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "src/styles/variables/colors";
+@import "src/styles/variables/shadows";
+
+.social-links {
+  display: flex;
+  width: 100%;
+  margin: 4vh 0;
+  padding: 4vh 0;
+  border-top: 4px dashed $color-primary-dark;
+  justify-content: space-around;
+
+  &__item {
+  }
+
+  &__link {
+    text-decoration: none;
+
+    &:focus,
+    &:hover {
+      transition: 0.4s;
+      animation: logo 0.4s forwards;
+      filter: drop-shadow($hover-shadow-dark);
+      fill: red;
+    }
+
+    &:last-of-type {
+      padding-right: 0;
+    }
+  }
+
+  &__image {
+    min-height: 1rem;
+    max-height: 3rem;
+    height: 10vw;
+    width: auto;
+  }
+}
+</style>
