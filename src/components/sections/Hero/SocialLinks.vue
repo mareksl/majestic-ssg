9291078bdef63f296b79fa8261bb93ebc1817c22
Marek Sladczyk <marek.sladczyk@stxnext.pl>
2020-09-12 22:58:41 +0200

    add svg-loader; social links

--- src/components/sections/Hero/SocialLinks.vue
+++ src/components/sections/Hero/SocialLinks.vue
@@ -6,22 +6,12 @@
         href="https://www.facebook.com/majesticreggae/"
         target="_blank"
       >
-        <g-image
-          class="social-links__image"
-          src="~/assets/icons/facebook-square.svg"
-          alt="Facebook"
-          title="Facebook"
-        ></g-image>
+        <IconFacebook class="social-links__icon" />
       </a>
     </li>
     <li class="social-links__item">
       <a class="social-links__link" href="https://spotify.com/" target="_blank">
-        <g-image
-          class="social-links__image"
-          src="~/assets/icons/spotify.svg"
-          alt="Spotify"
-          title="Spotify"
-        ></g-image>
+        <IconSpotify class="social-links__icon" />
       </a>
     </li>
     <li class="social-links__item">
@@ -30,20 +20,24 @@
         href="https://www.youtube.com/user/majesticreggae"
         target="_blank"
       >
-        <g-image
-          class="social-links__image"
-          src="~/assets/icons/youtube.svg"
-          alt="Youtube"
-          title="Youtube"
-        ></g-image>
+        <IconYoutube class="social-links__icon" />
       </a>
     </li>
   </ul>
 </template>
 
 <script>
+import IconFacebook from "@/assets/icons/facebook-square.svg";
+import IconSpotify from "@/assets/icons/spotify.svg";
+import IconYoutube from "@/assets/icons/youtube.svg";
+
 export default {
-  name: "SocialLinks"
+  name: "SocialLinks",
+  components: {
+    IconFacebook,
+    IconSpotify,
+    IconYoutube
+  }
 };
 </script>
 
@@ -59,29 +53,29 @@ export default {
   border-top: 4px dashed $color-primary-dark;
   justify-content: space-around;
 
-  &__item {
-  }
-
   &__link {
     text-decoration: none;
+    transition: 0.4s;
 
     &:focus,
     &:hover {
       transition: 0.4s;
       animation: logo 0.4s forwards;
       filter: drop-shadow($hover-shadow-dark);
-      fill: red;
-    }
 
-    &:last-of-type {
-      padding-right: 0;
+      .social-links__icon {
+        fill: $color-accent-light;
+      }
     }
   }
 
-  &__image {
-    min-height: 1rem;
-    max-height: 3rem;
-    height: 10vw;
+  &__icon {
+    display: block;
+    transition: 0.4s;
+    fill: $color-primary-dark;
+    min-height: 2rem;
+    max-height: 4rem;
+    height: 8vw;
     width: auto;
   }
 }
